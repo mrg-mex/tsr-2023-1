990e121aca51afa63c459ccfd799220ee4d571ef
Felipe Rivas <rivascf@gmail.com>
2022-11-17 18:13:47 -0600

    Ejemplo, algoritmo Go_to_Point finalizado

--- tb3_cmd/scripts/tb3_go2point.py
+++ tb3_cmd/scripts/tb3_go2point.py
@@ -14,11 +14,12 @@ class GoToPoint:
         self._distance_to_go = 0.0
         self._goal = Pose2D()
         self._phi = 0.0
-        self._tol_err_yaw = 0.0349066 # rads = 2 grados
-        self._tol_err_dist = 0.001
+        self._tol_err_yaw = 0.0872665 # rads = 5 grados
+        # 0.0349066 # rads = 2 grados
+        self._tol_err_dist = 0.05       #0.001
         self._ang_vel = 0.1
         self._lin_vel = 0.1
-        self._robot_state = 'STOP' # Variable de estado del robot
+        self._robot_state = 'STOP' # Variable de estado del robot [STOP, GO, TWIST, GOAL]
         self._odom_sub = rospy.Subscriber('/odom', Odometry, self._on_odometry_update)
         self._cmdvel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 
@@ -38,28 +39,34 @@ class GoToPoint:
     def _compute_goal(self):
         dx = (self._goal.x - self._pose_act.x)
         dy = (self._goal.y - self._pose_act.y)
-        dyaw = math.atan2(dy, dx)
+        phi = math.atan2(dy, dx)
         dif_dist = math.hypot(dx, dy)
-
+        dyaw = phi - self._pose_act.theta
         return dyaw, dif_dist
 
     def _head_towards_goal(self):
         goal_yaw, dist_to_goal = self._compute_goal()
-        rospy.loginfo(f'HEAD: Yaw err: {goal_yaw:.6f}, dist to go: {dist_to_goal:.6f}')
+        rospy.loginfo(f'HEADING: Yaw err: {goal_yaw:.6f}, dist to go: {dist_to_goal:.6f}')
         if math.fabs(goal_yaw) > self._tol_err_yaw:
             ang_vel = self._ang_vel if goal_yaw > 0 else -self._ang_vel
             # mandar el comando de giro al robot
-            self._send_vel_robot(vel_ang=ang_vel)
+            self._send_vel_robot(vel_ang=ang_vel, robot_state='TWIST')
+        else:
+            self._robot_state = 'GO'    
 
     def _go_staight(self):
         goal_yaw, dist_to_goal = self._compute_goal()    
         rospy.loginfo(f'GO: Yaw err: {goal_yaw:.6f}, dist to go: {dist_to_goal:.6f}')
-        if dist_to_goal > self._tol_err_dist:
-            self._send_vel_robot(vel_lin=self._lin_vel)
-        else:
-            self._send_vel_robot()
-        if math.fabs(goal_yaw) > self._tol_err_yaw:
-            self._head_towards_goal()    
+        if self._robot_state not in ['GOAL','STOP']:
+            if dist_to_goal > self._tol_err_dist:
+                self._send_vel_robot(vel_lin=self._lin_vel, robot_state='GO')
+            else: # Llegamos a la meta
+                self._robot_state = 'GOAL'
+                rospy.loginfo(f"GOAL!, yaw error: {goal_yaw:.6f}, dist error: {dist_to_goal:.6f}")
+                self._send_vel_robot()
+            if math.fabs(goal_yaw) > self._tol_err_yaw:
+                #self._head_towards_goal()    
+                self._robot_state = 'TWIST'
 
     def set_goal(self, x, y, theta):
         self._goal.x = x
@@ -67,18 +74,41 @@ class GoToPoint:
         self._goal.theta = theta
 
     
-    def _send_vel_robot(self, vel_ang = 0.0, vel_lin = 0.0):
+    def _send_vel_robot(self, vel_ang = 0.0, vel_lin = 0.0, robot_state='STOP'):
+        self._robot_state = robot_state
         cmd_twist =  Twist()
         cmd_twist.linear.x = vel_lin
         cmd_twist.angular.z = vel_ang
 
-        self._cmdvel_pub(cmd_twist)
+        self._cmdvel_pub.publish(cmd_twist)
 
     def start(self):
-        self._go_staight()    
+        self._robot_state = 'GO'
+
+    def getRobotState(self):
+        return self._robot_state
+
+    def stop(self):
+        self._send_vel_robot()
+
 
 if __name__ == "__main__":
     tb3_go2point = GoToPoint()
     tb3_go2point.set_goal(2, 3.5, 0)
-    tb3_go2point.start()
-    rospy.spin()
+    rate = rospy.Rate(1)
+    if tb3_go2point.getRobotState() == 'STOP':
+        tb3_go2point.start()
+
+    while (not rospy.is_shutdown()):
+        rospy.loginfo(f"Estado actual {tb3_go2point.getRobotState()}")
+        if tb3_go2point.getRobotState() == 'TWIST':
+            tb3_go2point._head_towards_goal()
+        elif tb3_go2point.getRobotState() == 'GO':
+            tb3_go2point._go_staight()
+        elif tb3_go2point.getRobotState() == 'GOAL':
+            tb3_go2point.stop()
+            rospy.loginfo(f"GOAL, posicion act x: {tb3_go2point._pose_act.x:.6f}, y: {tb3_go2point._pose_act.y:.6f}")
+            break
+
+        rate.sleep()
+    

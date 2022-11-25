74fb3e7769ce493f76edc0a7d5b98af027e18696
Felipe Rivas <rivascf@gmail.com>
2022-11-25 12:59:05 -0600

    Actualizacion al script tb3_go2point.py para desplegar el tiempo total empleado y terminar correctamente el proceso

--- tb3_cmd/scripts/tb3_go2point.py
+++ tb3_cmd/scripts/tb3_go2point.py
@@ -20,6 +20,7 @@ class GoToPoint:
         self._ang_vel = 0.1
         self._lin_vel = 0.1
         self._robot_state = 'STOP' # Variable de estado del robot [STOP, GO, TWIST, GOAL]
+        self._goal_reached = False
         self._odom_sub = rospy.Subscriber('/odom', Odometry, self._on_odometry_update)
         self._cmdvel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 
@@ -63,6 +64,7 @@ class GoToPoint:
             else: # Llegamos a la meta
                 self._robot_state = 'GOAL'
                 rospy.loginfo(f"GOAL!, yaw error: {goal_yaw:.6f}, dist error: {dist_to_goal:.6f}")
+                self._goal_reached = True
                 self._send_vel_robot()
             if math.fabs(goal_yaw) > self._tol_err_yaw:
                 #self._head_towards_goal()    
@@ -73,7 +75,6 @@ class GoToPoint:
         self._goal.y = y
         self._goal.theta = theta
 
-    
     def _send_vel_robot(self, vel_ang = 0.0, vel_lin = 0.0, robot_state='STOP'):
         self._robot_state = robot_state
         cmd_twist =  Twist()
@@ -90,25 +91,33 @@ class GoToPoint:
 
     def stop(self):
         self._send_vel_robot()
+        rospy.sleep(1)
 
+    def is_goal_reched(self):
+        return self._goal_reached
 
 if __name__ == "__main__":
     tb3_go2point = GoToPoint()
-    tb3_go2point.set_goal(2, 3.5, 0)
+    tb3_go2point.set_goal(3, 2, 0)
     rate = rospy.Rate(1)
+    start_time = rospy.Time.now()
     if tb3_go2point.getRobotState() == 'STOP':
         tb3_go2point.start()
-
     while (not rospy.is_shutdown()):
         rospy.loginfo(f"Estado actual {tb3_go2point.getRobotState()}")
         if tb3_go2point.getRobotState() == 'TWIST':
             tb3_go2point._head_towards_goal()
         elif tb3_go2point.getRobotState() == 'GO':
             tb3_go2point._go_staight()
-        elif tb3_go2point.getRobotState() == 'GOAL':
+        elif tb3_go2point.is_goal_reched():      # Cond. anterior: tb3_go2point.getRobotState() == 'GOAL'
+            end_time = rospy.Time.now()
+            elapse_time = end_time - start_time  
             tb3_go2point.stop()
-            rospy.loginfo(f"GOAL, posicion act x: {tb3_go2point._pose_act.x:.6f}, y: {tb3_go2point._pose_act.y:.6f}")
+            rospy.loginfo(f"On GOAL, posicion act x: {tb3_go2point._pose_act.x:.6f}, y: {tb3_go2point._pose_act.y:.6f}, theta: {tb3_go2point._pose_act.theta:.6f} rads.")
+            rospy.loginfo(f"Elpsed time: {elapse_time.to_sec():.4f} seg.")
             break
 
-        rate.sleep()
+    rospy.loginfo(f"Proceso concluido.")
+    
+    
     
